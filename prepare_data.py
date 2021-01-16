398085789bd4346cd986aaad8e05d6cf3807447f
Aron Ambrosiani <aron@ambrosiani.se>
2021-01-16 16:00:42 +0100

    add mean calculation when preparing data

--- prepare_data.py
+++ prepare_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 import csv
 import math
+import statistics
 
 from collections import Counter
 
@@ -28,24 +29,20 @@ authorData = {}
 keywordData = pd.DataFrame()
 
 for author in unique_authors:
-    authorData[author] = {'Name':author,'ArticlesTotal':0,'PagesTotal':0,'EarliestArticle':'9999','LatestArticle':'1','ActiveYears':0,'KeywordsUsed':[],'BornYear':'','Gender':''}
-
-#print (authorData)
-
-#for item in authorData:
-#    print(item)
+    authorData[author] = {'Name':author,'ArticlesTotal':0,'PagesTotal':0,'EarliestArticle':'9999','LatestArticle':'1','Articles':[],'ArticleMean':'','ActiveYears':0,'KeywordsUsed':[],'BornYear':'','Gender':''}
 
 for row in articleData.itertuples():
     authorList = getTokenList(row[2])
     keywordList = getTokenList(str(row[34]))
     for author in authorList:
         authorData[author]['ArticlesTotal'] = authorData[author]['ArticlesTotal'] +1
-        if not math.isnan(row[18]):
+        if not math.isnan(row[18]): #If page count exists
             authorData[author]['PagesTotal'] = authorData[author]['PagesTotal'] +row[18]
         if int(row[16]) < int(authorData[author]['EarliestArticle']):
             authorData[author]['EarliestArticle'] = row[16]
         if int(row[16]) > int(authorData[author]['LatestArticle']):
             authorData[author]['LatestArticle'] = row[16]
+        authorData[author]['Articles'].append(float(row[16])) # add article year to list
         for keyword in keywordList:
             if not keyword == 'nan':
                 authorData[author]['KeywordsUsed'].append(keyword)
@@ -55,10 +52,11 @@ authorDataAsList = []
 for item in authorData.items():
     item[1]['ActiveYears'] = int(item[1]['LatestArticle']) - int(item[1]['EarliestArticle']) + 1
     item[1]['EarliestArticle'] = str(item[1]['EarliestArticle'])+'-01-01'
-    item[1]['LatestArticle'] = str(item[1]['LatestArticle'])+'-12-31'
+    item[1]['LatestArticle'] = str(item[1]['LatestArticle'])+'-12-01'
     keywordsUsed=Counter(item[1]['KeywordsUsed']).most_common()
     item[1]['KeywordsUsed'] = keywordsUsed
-    #print(item[1])
+    item[1]['ArticleMean'] = statistics.mean(item[1]['Articles'])
+
     authorDataAsList.append(item[1])
 
 authorDataPanda = pd.DataFrame(authorDataAsList)
